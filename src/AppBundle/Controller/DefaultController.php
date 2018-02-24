9fca72668e8fec4c2a9a81cb90b2796474d3d4db
Marin <marindauriat@hotmail.com>
2018-02-24 20:53:42 +0100

    Few changes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -271,6 +271,12 @@ class DefaultController extends Controller
     				}
     			}
     			
+    			// TODO Check if the most recent election is a district election
+    			// Check if district election (specific case)
+    			if($election->getType()->getCode() == ElectionType::CODE_LEG) {
+    			    return $this->electionDistrictCityAction($request);
+    			}
+    			
     			return $this->redirectToRoute("app_election_city", array(
     					"election_id"	=> $election->getId(),
     					"city_id"		=> $city->getId(),
@@ -300,6 +306,7 @@ class DefaultController extends Controller
     	$resultsCity = array();
     	$department = null;
     	
+    	// Get department
     	if(!empty($request->get('department_code'))) {
     		$departmentCode = $request->get('department_code');
     		if($this->startsWith($departmentCode, "0")) {
@@ -308,8 +315,9 @@ class DefaultController extends Controller
     		
     		$department = $repoDepartment->findOneByDep($departmentCode);
     	}
-    	else
-    		$department = $repoDepartment->find($request->get("department_id"));
+    	else {
+    	    $department = $repoDepartment->find($request->get("department_id"));
+    	}
     	
     	if($department != null && $election != null) {
 
@@ -423,6 +431,7 @@ class DefaultController extends Controller
     	$results = array();
     	$city = null;
     	
+    	// Get city
     	if(!empty($request->get('city_code'))) {
 			
     		$cityCode = $request->get('city_code');
@@ -466,10 +475,16 @@ class DefaultController extends Controller
 				}
     		}
     	}
-    	else
-    		$city = $repoCity->find($request->get("city_id"));
+    	else {
+    	    $city = $repoCity->find($request->get("city_id"));
+    	}
     	
     	if($city != null && $election != null) {
+    	    
+    	    // Check if district election (specific case)
+    	    if($election->getType()->getCode() == ElectionType::CODE_LEG) {
+    	        return $this->electionDistrictCityAction($request);
+    	    }
 
     		$results = $repoResultCity->createQueryBuilder("rc")
     		->join("rc.round", "r")
@@ -530,6 +545,158 @@ class DefaultController extends Controller
     		'previousElection'	=> $previousElection,
     		'nextElection'		=> $nextElection
     	));
+    }
+    
+    public function electionDistrictCityAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repoCity = $em->getRepository("AppBundle:City");
+        $repoDistrict = $em->getRepository("AppBundle:District");
+        $repoElection = $em->getRepository("AppBundle:Election");
+        $repoDepartment = $em->getRepository("AppBundle:Department");
+        $repoResultDistrictCity = $em->getRepository("AppBundle:ResultDistrictCity");
+        $election = $repoElection->find($request->get("election_id"));
+        $previousElection = null;
+        $nextElection = null;
+        $results = array();
+        $city = null;
+        $district = null;
+        
+        // Get city
+        if(!empty($request->get('city_code'))) {
+            
+            $cityCode = $request->get('city_code');
+            
+            // Get department code
+            $departmentCode = substr($cityCode, 0, 2);
+            if($this->startsWith($cityCode, "0")) {
+                $departmentCode = substr($departmentCode, 1, strlen($departmentCode)-1);
+            }
+            
+            $department = $repoDepartment->findOneByDep($departmentCode);
+            
+            if($department != null) {
+                
+                $cityCode = substr($cityCode, 2);
+                
+                // Get city code
+                if($this->startsWith($cityCode, "0")) {
+                    $cityCode = substr($cityCode, 1, strlen($cityCode)-1);
+                    
+                    if($this->startsWith($cityCode, "0")) {
+                        $cityCode = substr($cityCode, 1, strlen($cityCode)-1);
+                    }
+                }
+                
+                //$city = $repoCity->findOneBy(array("department" => $department, "com" => $cityCode));
+                
+                // Search city
+                $query = $repoCity->createQueryBuilder('c');
+                $query->where($query->expr()->notIn("c.actual", ":actual"))
+                ->andWhere($query->expr()->eq("c.department", ":department"))
+                ->andWhere($query->expr()->eq("c.com", ":com"))
+                ->setParameter('actual', array(City::ACTUAL_ANCIEN_CODE_CHGT_DEP, City::ACTUAL_FRACTION_CANTONALE))
+                ->setParameter("department", $department)
+                ->setParameter("com", $cityCode);
+                
+                $cities = $query->getQuery()->getResult();
+                
+                if(count($cities) > 0) {
+                    $city = $cities[0];
+                }
+            }
+        }
+        else {
+            $city = $repoCity->find($request->get("city_id"));
+        }
+        
+        // Get district
+        if(!empty($request->get('district_id'))) {
+            $district = $repoDistrict->find($request->get('district_id'));
+            
+            // Check if the district is one of the city's districts
+            if($district != null && $city != null && $district->getCity()->getId() != $city->getId()) {
+                $district = null;
+            }
+        }
+        
+        if(empty($request->get('district_id')) && $city != null) {
+            if(count($city->getDistrictsCities()) == 1) {
+                $district = $city->getDistrictsCities()[0]->getDistrict();
+            }
+        }
+        
+        if($city != null && $election != null && $district != null) {
+            
+            $results = $repoResultDistrictCity->createQueryBuilder("rdc")
+            ->join("rdc.round", "r")
+            ->join("rdc.districtCity", "dc")
+            ->select("rdc")
+            ->where("r.election = :election")
+            ->andWhere("dc.city = :city")
+            ->andWhere("dc.district = :district")
+            ->orderBy("r.roundNumber", "DESC")
+            ->setParameter("election", $election)
+            ->setParameter("city", $city)
+            ->setParameter("district", $district)
+            ->getQuery()
+            ->getResult();
+            
+            if(count($election->getRounds()) != 0) {
+                
+                $previousElection = $repoElection->createQueryBuilder("e")
+                ->join("e.rounds", "r")
+                ->select("e")
+                ->join("r.resultsDistrictCity", "rdc")
+                ->join("rdc.districtCity", "dc")
+                ->where("e.type = :type")
+                ->andWhere("r.roundNumber = 1")
+                ->andWhere("dc.city = :city")
+                ->andWhere("dc.district = :district")
+                ->andWhere("r.date < :date")
+                ->andWhere("e != :election")
+                ->setParameter("type", $election->getType())
+                ->setParameter("city", $city)
+                ->setParameter("district", $district)
+                ->setParameter("date", $election->getRounds()[0]->getDate())
+                ->setParameter("election", $election)
+                ->orderBy("r.date", "DESC")
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getOneOrNullResult();
+                
+                $nextElection = $repoElection->createQueryBuilder("e")
+                ->join("e.rounds", "r")
+                ->select("e")
+                ->join("r.resultsDistrictCity", "rdc")
+                ->join("rdc.districtCity", "dc")
+                ->where("e.type = :type")
+                ->andWhere("r.roundNumber = 1")
+                ->andWhere("dc.city = :city")
+                ->andWhere("dc.district = :district")
+                ->andWhere("r.date > :date")
+                ->andWhere("e != :election")
+                ->setParameter("type", $election->getType())
+                ->setParameter("city", $city)
+                ->setParameter("district", $district)
+                ->setParameter("date", $election->getRounds()[0]->getDate())
+                ->setParameter("election", $election)
+                ->orderBy("r.date", "ASC")
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getOneOrNullResult();
+            }
+            
+        }
+        
+        return $this->render('AppBundle:Default:election_district_city.html.twig', array(
+            'city'				=> $city,
+            'election'			=> $election,
+            'district'          => $district,
+            'results'			=> $results,
+            'previousElection'	=> $previousElection,
+            'nextElection'		=> $nextElection
+        ));
     }
     
     public function electionDistrictAction(Request $request)
